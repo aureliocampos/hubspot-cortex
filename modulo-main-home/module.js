54c1e1263d5ae806e458ca6e610830201a9b81a7
Aurélio Campos <aureliocampos.dev@gmail.com>
2021-09-19 19:31:15 -0300

    Ajustando animação main-home

---
+++ modulo-main-home/module.js
@@ -0,0 +1,74 @@
+const solutionCards = Array.from(document.querySelector('.main-home__cards').children)
+let isPaused = false
+let time = 0
+
+function createAnimationCard(card) {
+
+  const current = card
+  const video = document.querySelector(`#${current.dataset.target}`)
+  video.classList.add('active')
+  video.load();
+
+  const progressBar = card.firstElementChild
+
+  const progressBarAnimation = progressBar.animate(
+    {
+      width: ["0%", "100%"] 
+    },
+    {
+      duration: 8000,
+      pseudoElement: "::after",
+    },
+  )
+  current.addEventListener('mouseenter', () => {
+    video.pause();
+    progressBarAnimation.pause();
+    isPaused = true;
+  })
+
+  current.addEventListener('mouseleave', () => {
+    video.play();
+    progressBarAnimation.play();
+    isPaused = false;
+  })
+
+}
+const allCards = solutionCards
+
+allCards[0].classList.add('active')
+createAnimationCard(allCards[0])
+
+  
+function displayTime() {
+
+  if (allCards[0].classList.contains('active')) {
+
+    allCards[0].classList.remove('active')
+    const video = document.querySelector(`#${allCards[0].dataset.target}`)
+    video.classList.remove('active')
+    
+    allCards[1].classList.add('active')
+    createAnimationCard(allCards[1])
+    return
+  }
+
+  if (allCards[1].classList.contains('active')) {
+
+    allCards[1].classList.remove('active')
+    const video = document.querySelector(`#${allCards[1].dataset.target}`)
+    video.classList.remove('active')
+
+    allCards[0].classList.add('active')
+    createAnimationCard(allCards[0])
+
+    return
+  }
+
+}
+
+const createClock = window.setInterval(function() {
+  if(!isPaused) {
+    time++;
+    displayTime()
+  }
+}, 8000);
---
+++ src/sections/pages/solutions/customer-testimonial.module/module.js
@@ -0,0 +1,3 @@
+const doc = $('.customer-testimonial');
+
+console.log(doc);
