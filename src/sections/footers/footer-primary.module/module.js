51e7a0202cdf6c58ce72e58dd6d87f532672a0af
Aurélio Campos <aureliocampos.dev@gmail.com>
2021-09-29 13:11:03 -0300

    Fechando template de soluções

--- src/sections/footers/footer-primary.module/module.js
+++ src/sections/footers/footer-primary.module/module.js
@@ -1,8 +1,11 @@
-setTimeout(() => {
-  const buttonSubmit = document.querySelector('.footer__form form .actions');
+function addArrowInButtonOfNewsletter() {
+  setTimeout(() => {
+    const buttonSubmit = document.querySelector('.footer__form form .actions');
+    buttonSubmit.innerHTML += '<span class="icon-input-seta"></span>'
+    
+  }, 1000);
+}
 
-  buttonSubmit.innerHTML += '<span class="icon-input-seta"></span>'
-  
-}, 2000);
+document.addEventListener("DOMContentLoaded", addArrowInButtonOfNewsletter)
 
 
