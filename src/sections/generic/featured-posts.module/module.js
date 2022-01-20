ccd080f092995b3dc43cbc6687db38b0a3035a33
Aurélio Campos <aureliocampos.dev@gmail.com>
2022-01-19 23:58:26 -0300

    Add featured posts section

---
+++ src/sections/generic/featured-posts.module/module.js
@@ -0,0 +1,24 @@
+const swiperResources = new Swiper('.featured-posts__swiper', {
+  loop:true,
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  }
+});
