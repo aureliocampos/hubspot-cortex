d81f31d3d8cf36d697be77265e108720fed2bfe6
Aurélio Campos <aureliocampos.dev@gmail.com>
2022-03-29 11:32:24 -0300

    feature: Adicionando novos itens ao filtros da pltaforma

--- src/js/materials.js
+++ src/js/materials.js
@@ -12,3 +12,47 @@ const toggleFilter = button => {
 
 toggleFilter(buttonOpenFilter)
 toggleFilter(buttonClosedFilter)
+
+
+// $(document).ready(function(){
+//   $(function () {
+//       $(".blog-hidden").slice(0, 6).show();
+//       $("#button__loadmore").on('click', function (e) {
+//           e.preventDefault();
+//           $(".blog-hidden:hidden").slice(0, 6).slideDown();
+//           if ($(".blog-hidden:hidden").length == 0) {
+//               $("#load").fadeOut('slow');
+//           }
+//       });
+//   });
+// });
+
+
+const loadMoreArticles = () => {
+  const getArticles = Array.from(document.querySelectorAll(".blog-hidden"));
+  const articlesOffset = getArticles.slice(0, 6);
+  const loadmoreButton = document.querySelector("#button__loadmore");
+
+  articlesOffset.forEach( item => {
+    item.classList.remove("blog-hidden")
+    item.style.display = "block"
+  })
+
+  loadmoreButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const getArticles = Array.from(document.querySelectorAll(".blog-hidden"));
+    const articlesOffset = getArticles.slice(0, 6);
+
+    articlesOffset.forEach( item => {
+      item.classList.remove("blog-hidden")
+      item.style.display = "block"
+    })
+
+    if (!getArticles) {
+      loadmoreButton.style.display = "none"
+    }
+  })
+
+}
+loadMoreArticles()
+
